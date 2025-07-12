fefb7adc1c04844bf3628b3da2716520078c98d5
rakes singh <rakeshdevcode@gmail.com>
2025-07-12 17:27:23 +0530

    TMDB API data Fetch &&  built maincontainer

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,13 +1,28 @@
-import React from 'react'
-import Header from "./Header"
+import Header from "./Header";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
+
+  useNowPlayingMovies();
+ 
   return (
     <div>
-      <Header/>
-      
-    </div>  
-  )
-}
+      <Header />
+      <MainContainer/>
+      <SecondaryContainer/>
+      {/* MainContainer
+          - Video Background
+          - VideoTitle
+          Secondary Container
+          - Movies List *n
+            - cards *n
+       */}
+    </div>
+  );
+};
 
-export default Browse
+export default Browse;
+
+
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,11 @@ export const LOGO="logo.png";
 export const USER_AVATAR="user-icon.jpg";
 
 export const BackGroundImage="/bg.jpg";
+
+export const API_OPTIONS =  {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MTk3MjQ1ZjNhMjQ4NzQ2MzZmYjdjNDRmMGIxMTU4OCIsIm5iZiI6MTc1MjIzMDE1NC40OTcsInN1YiI6IjY4NzBlOTBhYTFhYWI0OTRiYzcwNDgwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RZxnbg7Irn4CDTC4ryccVc6zISePfCK_yKrEZGF8rw4'
+  }
+};
