bdfdab6ad6226c2b23971c01b3051e8810834429
rakes singh <rakeshdevcode@gmail.com>
2025-07-18 18:11:01 +0530

    security update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,8 +8,7 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MTk3MjQ1ZjNhMjQ4NzQ2MzZmYjdjNDRmMGIxMTU4OCIsIm5iZiI6MTc1MjIzMDE1NC40OTcsInN1YiI6IjY4NzBlOTBhYTFhYWI0OTRiYzcwNDgwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RZxnbg7Irn4CDTC4ryccVc6zISePfCK_yKrEZGF8rw4",
+    Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
   },
 };
 
@@ -20,5 +19,3 @@ export const SUPPORTED_LANGUAGE = [
   { identifier: "hindi", name: "Hindi" },
   { identifier: "spanish", name: "Spanish" },
 ];
-
-//  export const OPENAI_KEY =  "";
